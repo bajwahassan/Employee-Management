0a61e0a9d5d9e98e05f80c6241984884a86cf4dd
hbajwaa <bajwahassan3@outlook.com>
2020-05-03 02:26:44 -0400

    cleaned up some code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
-
     set :public_folder, 'public'
     # set :views, 'app/views'
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions
     set :session_secret, 'password_secure'
-  
 
 
     def logged_in?
@@ -17,9 +15,5 @@ class ApplicationController < Sinatra::Base
     def current_user
       Organization.find(session[:user_id])
     end
-
-
-
-
 
 end
