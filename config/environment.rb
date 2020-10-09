c61313dbd9edc8481f8c8f8aa33bb7694421ccb8
hbajwaa <bajwahassan3@outlook.com>
2020-10-09 00:55:16 -0400

    added pg

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,15 @@
-require 'bundler'
-Bundler.require
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'db/development.sqlite'
-)
-require_all 'app'
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
+
 configure :production do
    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
@@ -17,3 +22,5 @@ configure :production do
      :encoding => 'utf8'
      )
 end
+
+require_all 'app'
